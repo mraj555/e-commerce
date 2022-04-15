2ca6551d6d102358f0311a6356cb3a9275f15f8b
mraj555 <jenilambaliya999@gmail.com>
2022-04-15 22:07:37 +0530

    Verify Email

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:project/User%20Details/decision_tree.dart';
-import 'package:project/User%20Details/log_in.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,16 +18,19 @@ void main() async {
 }
 
 // void main() async {
-// WidgetsFlutterBinding.ensureInitialized();
-// await Firebase.initializeApp();
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
 //   runApp(
 //     DevicePreview(
 //       enabled: true,
 //       builder: (BuildContext context) =>
 //           MaterialApp(
-//           useInheritedMediaQuery: true,
-//           debugShowCheckedModeBanner: false,
-//           home: SplashScreen(),
-//     ),
-//   ),);
+//             routes: {
+//               '/signupdecision': (context) => const DecisionTree(),
+//             },
+//             useInheritedMediaQuery: true,
+//             debugShowCheckedModeBanner: false,
+//             home: const DecisionTree(),
+//           ),
+//     ),);
 // }
